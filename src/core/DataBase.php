6d104b1d5fe9b857cbdfadc282656a50a1fef0dd
Vigitoreanec <chirik39@mail.ru>
2025-01-25 20:43:12 +0200

    TSingletone реализовали отдельно

--- src/core/DataBase.php
+++ src/core/DataBase.php
@@ -15,20 +15,6 @@ class DataBase
         'database' => 'database.db'
     ];
 
-    private function __construct() {}
-    private function __clone() {}
-    private function __wakeup() {}
-
-    private static ?DataBase $instance = null;
-
-    public static function getInstance()
-    {
-        if (is_null(static::$instance)) {
-            static::$instance = new static();
-        }
-        return static::$instance;
-    }
-
     private ?\PDO $connection = null;
 
     private function getConnection(): PDO
---
+++ traits/TSingletone.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace traits;
+
+use Sergey\Oop\core\DataBase;
+
+
+trait TSingletone
+{
+    private function __construct() {}
+    private function __clone() {}
+    private function __wakeup() {}
+
+    private static ?DataBase $instance = null;
+
+    public static function getInstance()
+    {
+        if (is_null(static::$instance)) {
+            static::$instance = new static();
+        }
+        return static::$instance;
+    }
+}
